db392d331e636937419fb622ac15de9fb594ed81
Hasan75 <hasanjab14@gmail.com>
2021-09-29 03:02:46 +0600

    Meal Compinewnt added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Restaurant from './components/Restaurant/Restaurant';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Restaurant></Restaurant>
     </div>
   );
 }
