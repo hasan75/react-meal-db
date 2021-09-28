6dbc18d48869bd1416136541d2aa83ca09527e3e
Hasan75 <hasanjab14@gmail.com>
2021-09-29 02:56:36 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to My Restaurant</h2>
+            <h4>Please order the food you like</h4>
+        </div>
+    );
+};
+
+export default Header;
